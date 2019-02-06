efd71bea171e5eeea652f704d73ff16857105500
Alessandro Steri <alessandro.steri@gmail.com>
2019-02-06 11:51:01 +0100

    switched to gradient as loss

--- exec.sh
+++ exec.sh
@@ -1,1 +1,1 @@
-time python3 main.py 2 16 glove840 300 0.001 200 256 --max_len_p=40 --max_len_h=27 --dev_env --with_img --gpu --trainable
+time python3 main.py 40 32 glove840 300 0.001 200 256 --max_len_p=40 --max_len_h=27 --dev_env --with_img --gpu --trainable
