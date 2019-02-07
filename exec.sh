25ebed5cf75ed7e09528db76b5b8fc3d46d46e97
Alessandro Steri <alessandro.steri@gmail.com>
2019-02-07 22:03:45 +0100

    add tb log for attention and dep

--- exec.sh
+++ exec.sh
@@ -1,1 +1,1 @@
-time python3 main.py --max_len_p=40 --max_len_h=27 --dev_env --with_img2 --with_DEP --trainable --attentive_my
+time python3 main.py --step_check=500 --max_len_p=40 --max_len_h=27 --dev_env --with_img2 --with_DEP --trainable --attentive_my --attentive
