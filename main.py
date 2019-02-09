15ff4154b9ff879f42dafb54fe3fa53727012d0b
Agostina Calabrese <calabrese.1657689@studenti.uniroma1.it>
2019-02-09 21:41:31 +0100

    Ops I forgot to update main.

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ from gte.model import GroundedTextualEntailmentModel
 from gte.utils.log import id_gen
 from gte.utils.dic import index_map
 from gte.info import MAX_LEN_P, MAX_LEN_H, SHUFFLED_DIR, DEP_REL
-from gte.preprocessing.dataset import datasets_to_word_set, words_to_dictionary, generate_shuffled_datasets
+from gte.preprocessing.dataset import datasets_to_word_set, words_to_dictionary, generate_shuffled_datasets, generate_datasets_with_dependency
 from gte.images import Image2vec
+from collections import Counter
 
 
 def main(options, ID):
     if not os.path.exists(SHUFFLED_DIR): generate_shuffled_datasets()
 
     words, labels, lens_p, lens_h = datasets_to_word_set()
+    #print(sorted(Counter(lens_p).items()))
+    #print("\n", sorted(Counter(lens_h).items()))
     # print("Words: ", len(words))
     # print("Labels: ", labels)
 
@@ -57,11 +60,14 @@ if __name__ == '__main__':
     cmdLineParser.add_argument("--with_img", dest="with_img", action='store_true', help="Makes use of bilateral matching.")
     cmdLineParser.add_argument("--with_img2", dest="with_img2", action='store_true', help="Makes use of our matching.")
     cmdLineParser.add_argument("--with_DEP", dest="with_DEP", action='store_true', help="Makes use of Dependency Tree.")
+    cmdLineParser.add_argument("--wo_SW", dest="wo_SW", action='store_true', help="Removes Stopwords.")
     cmdLineParser.add_argument("--dropout", dest="dropout", action='store_true', help="Applies dropout at the latent representation.")
     cmdLineParser.add_argument("--attentive", dest="attentive", action='store_true', help="Applies cross multihead attention to PI vs PI and HI vs HI.")
     cmdLineParser.add_argument("--attentive_I", dest="attentive_I", action='store_true', help="Applies cross multihead attention to I vs P and I vs H.")
     cmdLineParser.add_argument("--attentive_my", dest="attentive_my", action='store_true', help="Applies cross my attention to PI vs HI and HI vs PI.")
     cmdLineParser.add_argument("--attentive_swap", dest="attentive_swap", action='store_true', help="Applies cross multihead attention to PI vs HI and HI vs PI.")
+    cmdLineParser.add_argument("--with_cos_PH", dest="with_cos_PH", action='store_true', help="Use cosine similarity between context_p and context_h.")
+    cmdLineParser.add_argument("--with_top_down", dest="with_top_down", action='store_true', help="Use top down image attention.")
     options = cmdLineParser.parse_args()
     # }}}
 
@@ -86,6 +92,9 @@ if __name__ == '__main__':
     if options.attentive_I: model_info += 'AI'.format()
     if options.attentive_my: model_info += 'Amy'.format()
     if options.with_DEP: model_info += 'Dep'.format()
+    if options.wo_SW: model_info += '.SWlower'.format()
+    if options.with_cos_PH: model_info += '.cosPH'.format()
+    if options.with_top_down: model_info += '.topDown'.format()
 
     exe_id = id_gen() # sort-of unique and monotonic id for tensorboard and logging
     ID = exe_id + env + model_info
