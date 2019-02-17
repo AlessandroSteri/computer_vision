9ae9885f9ac3a9b4b9d7561bfa450202daf8c3a0
Alessandro <steri.1356261@studenti.uniroma1.it>
2019-02-17 20:49:08 +0100

    multihead seq match

--- gte/model/model.py
+++ gte/model/model.py
@@ -90,7 +90,7 @@ class GroundedTextualEntailmentModel(object):
         self.input_layer()
         self.embedding_layer()
         self.context_layer()
-        #self.sequence_matching(self.options.hidden_size)
+        self.sequence_matching_mutihead_attentive(self.options.hidden_size)
 
         #self.sequence_matching_with_top_down(self.options.hidden_size)
 
@@ -102,7 +102,7 @@ class GroundedTextualEntailmentModel(object):
 
         #self.relation_networks_ranker()
 
-        self.sequence_matching_with_top_down_multi_learning(self.options.hidden_size)
+        # self.sequence_matching_with_top_down_multi_learning(self.options.hidden_size)
 
         self.prediction_layer()
 
@@ -181,6 +181,52 @@ class GroundedTextualEntailmentModel(object):
     def sequence_matching(self, n):
         # import ipdb; ipdb.set_trace()  # TODO BREAKPOINT
         p = tf.concat([self.P_states[0][0], self.P_states[0][1]], -1)  # [B, 2*H]
+        q = tf.concat([self.H_states[0][0], self.H_states[0][1]], -1)  # [B, 2*H]
+        # p_shape = tf.shape(p)
+        # q_shape = tf.shape(q)
+        # p = tf.reshape(p, (-1, 2*self.options.hidden_size))
+        # q = tf.reshape(q, (-1, 2*self.options.hidden_size))
+        q_sub_p = p - q  # TODO try with module
+        q_mul_p = tf.multiply(p , q)
+        p = tf.expand_dims(p, axis=1)
+        q = tf.expand_dims(q, axis=1)
+        q_sub_p = tf.expand_dims(q_sub_p, axis=1)
+        q_mul_p = tf.expand_dims(q_mul_p, axis=1)
+        x_cl = tf.concat([p, q, q_sub_p, q_mul_p], 1)  # [B, 4, HH] #TODO add max e min pointwise
+        x_cl_flat = tf.reshape(x_cl, (self.options.batch_size*4, 2*self.options.hidden_size))
+        W_z= tf.get_variable("W_z", [n, 2*self.options.hidden_size], dtype=tf.float32)
+        b_z = tf.get_variable("b_z", [n], dtype=tf.float32)
+        z = tf.transpose(tf.matmul(W_z, tf.transpose(x_cl_flat))) + b_z
+        z = tf.reshape(z, (self.options.batch_size, -1))
+        W_score= tf.get_variable("W_score", [NUM_CLASSES, 4*n], dtype=tf.float32)
+        b_score = tf.get_variable("b_score", [NUM_CLASSES], dtype=tf.float32)
+        # score = tf.nn.xw_plus_b(tf.nn.relu(z), W_score, b_score)
+        self.score = tf.transpose(tf.matmul(W_score, tf.transpose(tf.nn.relu(z)))) + b_score
+
+        self.prob = tf.nn.softmax(self.score)
+        gold_matrix = tf.one_hot(self.labels, NUM_CLASSES, dtype=tf.float32)
+        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.score, labels=gold_matrix))
+        clipper = 50
+        optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+        tvars = tf.trainable_variables()
+        l2_loss = tf.add_n([tf.nn.l2_loss(v) for v in tvars if v.get_shape().ndims > 1])
+        self.loss = self.loss + 1e-5 * l2_loss
+        grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars), clipper)
+        if self.options.decay:
+            self.optimizer = optimizer.apply_gradients(zip(grads, tvars), global_step=self.global_step)
+        else:
+            self.optimizer = optimizer.apply_gradients(zip(grads, tvars))
+
+        loss_summ = tf.summary.scalar('loss', self.loss)
+        self.train_summary.append(loss_summ)
+
+    def sequence_matching_mutihead_attentive(self, n):
+        p = tf.concat([self.P_states[0][0], self.P_states[0][1]], -1)  # [B, 2*H]
+        p_shape = tf.shape(p)
+        p = tf.reshape(p, (self.options.batch_size, 2, self.options.hidden_size))
+        p = multihead_attention(p, self.I, num_heads=8, is_training=self.is_training, scope="P_attention")
+        p = tf.reshape(p, p_shape)
+        # import ipdb; ipdb.set_trace()  # TODO BREAKPOINT
         q = tf.concat([self.H_states[0][0], self.H_states[0][1]], -1)  # [B, 2*H]
         # p_shape = tf.shape(p)
         # q_shape = tf.shape(q)
