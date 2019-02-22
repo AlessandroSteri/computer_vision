c641707121e6760c749313e817e01ce5bfa106d4
Alessandro Steri <alessandro.steri@gmail.com>
2019-02-22 10:49:30 +0100

    merge ub desktop and solved conflicts

--- exec.sh
+++ exec.sh
@@ -1,1 +1,1 @@
-time python3 main.py --dropout --sequence_matching=min_max --gpu --step_check=500 --max_len_p=20 --max_len_h=14 --dev_env --trainable --with_top_down --with_P_top_down --learning_rate 0.0001 --epoch 300 # --restore # --with_DEP --wo_SW #--attentive_my --attentive --with_cos_PH --with_img2
+time python3 main.py --dropout --sequence_matching=no_p_split --gpu --step_check=500 --dev_env --trainable --with_top_down --with_P_top_down --learning_rate 0.0001 --epoch 300 # --restore # --with_DEP --wo_SW #--attentive_my --attentive --with_cos_PH --with_img2
--- gte/model/model.py
+++ gte/model/model.py
@@ -91,7 +91,9 @@ class GroundedTextualEntailmentModel(object):
         self.embedding_layer()
         self.context_layer()
 
-        if self.options.sequence_matching == 'sequence_matching':
+        if self.options.with_top_down:
+            self.image_top_down_attention_later()
+        elif self.options.sequence_matching == 'sequence_matching':
             self.sequence_matching(self.options.hidden_size)
         elif self.options.sequence_matching == 'min_max':
             self.sequence_matching_min_max(self.options.hidden_size)
@@ -109,11 +111,17 @@ class GroundedTextualEntailmentModel(object):
             self.sequence_matching_no_p(self.options.hidden_size)
         elif self.options.sequence_matching == 'no_p_top_down':
             self.sequence_matching_no_p_top_down(self.options.hidden_size)
-        if self.options.with_top_down:
-            self.image_top_down_attention_later()
+        elif self.options.sequence_matching == 'no_p_split':
+            self.sequence_matching_no_p_split(self.options.hidden_size)
+        elif self.options.sequence_matching == 'no_p_split_hw':
+            self.sequence_matching_no_p_split_hw(self.options.hidden_size)
+        elif self.options.sequence_matching == 'rel_ranker':
+            self.relation_networks_ranker()
         else: assert False
-
         #if self.options.with_top_down: self.image_top_down_attention_later()
+
+        # if self.options.with_top_down: self.image_top_down_attention_later()
+        # if self.options.with_top_down: self.image_top_down_attention_later()
         # if self.options.with_matching: self.bilateral_matching_layer()
         # else: self.matching_layer()
         # if not self.options.with_top_down:
@@ -291,6 +299,222 @@ class GroundedTextualEntailmentModel(object):
         W_i = tf.get_variable("w_i", [FEAT_SIZE*NUM_FEATS, 2*self.options.hidden_size], dtype=tf.float32)
         b_i = tf.get_variable("b_i", [2*self.options.hidden_size], dtype=tf.float32)
         p = tf.matmul(p, W_i) + b_i
+
+        q = tf.concat([self.H_states[0][0], self.H_states[0][1]], -1)  # [B, 2*H]
+        # p_shape = tf.shape(p)
+        # q_shape = tf.shape(q)
+        # p = tf.reshape(p, (-1, 2*self.options.hidden_size))
+        # q = tf.reshape(q, (-1, 2*self.options.hidden_size))
+        q_sub_p = p - q  # TODO try with module
+        q_mul_p = tf.multiply(p , q)
+        p = tf.expand_dims(p, axis=1)
+        q = tf.expand_dims(q, axis=1)
+        q_sub_p = tf.expand_dims(q_sub_p, axis=1)
+        q_mul_p = tf.expand_dims(q_mul_p, axis=1)
+        x_cl = tf.concat([p, q, q_sub_p, q_mul_p], 1)  # [B, 4, HH] #TODO add max e min pointwise
+        x_cl_flat = tf.reshape(x_cl, (self.options.batch_size*4, 2*self.options.hidden_size))
+        W_z= tf.get_variable("W_z", [n, 2*self.options.hidden_size], dtype=tf.float32)
+        b_z = tf.get_variable("b_z", [n], dtype=tf.float32)
+        z = tf.transpose(tf.matmul(W_z, tf.transpose(x_cl_flat))) + b_z
+        z = tf.reshape(z, (self.options.batch_size, -1))
+        W_score= tf.get_variable("W_score", [NUM_CLASSES, 4*n], dtype=tf.float32)
+        b_score = tf.get_variable("b_score", [NUM_CLASSES], dtype=tf.float32)
+        # score = tf.nn.xw_plus_b(tf.nn.relu(z), W_score, b_score)
+        self.score = tf.transpose(tf.matmul(W_score, tf.transpose(tf.nn.relu(z)))) + b_score
+
+        self.prob = tf.nn.softmax(self.score)
+        gold_matrix = tf.one_hot(self.labels, NUM_CLASSES, dtype=tf.float32)
+        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.score, labels=gold_matrix))
+        clipper = 50
+        optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+        tvars = tf.trainable_variables()
+        l2_loss = tf.add_n([tf.nn.l2_loss(v) for v in tvars if v.get_shape().ndims > 1])
+        self.loss = self.loss + 1e-5 * l2_loss
+        grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars), clipper)
+        if self.options.decay:
+            self.optimizer = optimizer.apply_gradients(zip(grads, tvars), global_step=self.global_step)
+        else:
+            self.optimizer = optimizer.apply_gradients(zip(grads, tvars))
+
+        loss_summ = tf.summary.scalar('loss', self.loss)
+        self.train_summary.append(loss_summ)
+
+    def sequence_matching_no_p_split_hw(self, n):
+        # p = tf.concat([self.P_states[0][0], self.P_states[0][1]], -1)  # [B, 2*H]
+
+        I_split = tf.split(self.I, 7, 1)
+        I_context = []
+        for i in range(7):
+            context, state = self.directional_lstm(I_split[i], [7 for _ in range(self.options.batch_size)], 2, self.options.hidden_size, 1, 'I_context')
+            state = tf.concat([state[0][0], state[0][1]], -1) # fw + bw
+            # split_contextual_repr = tf.concat(context, -1)
+            I_context.append(tf.expand_dims(state, 1))
+        # import ipdb; ipdb.set_trace()  # TODO BREAKPOINT
+
+        I_full_context = tf.concat(I_context, 1) #B, 7, HH
+        _, I_embedding = self.directional_lstm(I_split[i], [7 for _ in range(self.options.batch_size)], 2, self.options.hidden_size, 1, 'I_embedding')
+        p = tf.concat([I_embedding[0][0], I_embedding[0][1]], -1)  # [B, 2*H]
+        # p = tf.reshape(p, (self.options.batch_size, -1))
+        #TODO remove W b
+        # W_i = tf.get_variable("w_i", [FEAT_SIZE, 2*self.options.hidden_size], dtype=tf.float32)
+        # b_i = tf.get_variable("b_i", [2*self.options.hidden_size], dtype=tf.float32)
+        # p = tf.matmul(p, W_i) + b_i
+        p = highway(p, FEAT_SIZE, tf.nn.relu)
+
+        q = tf.concat([self.H_states[0][0], self.H_states[0][1]], -1)  # [B, 2*H]
+        # p_shape = tf.shape(p)
+        # q_shape = tf.shape(q)
+        # p = tf.reshape(p, (-1, 2*self.options.hidden_size))
+        # q = tf.reshape(q, (-1, 2*self.options.hidden_size))
+        q_sub_p = p - q  # TODO try with module
+        q_mul_p = tf.multiply(p , q)
+        p = tf.expand_dims(p, axis=1)
+        q = tf.expand_dims(q, axis=1)
+        q_sub_p = tf.expand_dims(q_sub_p, axis=1)
+        q_mul_p = tf.expand_dims(q_mul_p, axis=1)
+        x_cl = tf.concat([p, q, q_sub_p, q_mul_p], 1)  # [B, 4, HH] #TODO add max e min pointwise
+        x_cl_flat = tf.reshape(x_cl, (self.options.batch_size*4, 2*self.options.hidden_size))
+        W_z= tf.get_variable("W_z", [n, 2*self.options.hidden_size], dtype=tf.float32)
+        b_z = tf.get_variable("b_z", [n], dtype=tf.float32)
+        z = tf.transpose(tf.matmul(W_z, tf.transpose(x_cl_flat))) + b_z
+        z = tf.reshape(z, (self.options.batch_size, -1))
+        W_score= tf.get_variable("W_score", [NUM_CLASSES, 4*n], dtype=tf.float32)
+        b_score = tf.get_variable("b_score", [NUM_CLASSES], dtype=tf.float32)
+        # score = tf.nn.xw_plus_b(tf.nn.relu(z), W_score, b_score)
+        self.score = tf.transpose(tf.matmul(W_score, tf.transpose(tf.nn.relu(z)))) + b_score
+
+        self.prob = tf.nn.softmax(self.score)
+        gold_matrix = tf.one_hot(self.labels, NUM_CLASSES, dtype=tf.float32)
+        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.score, labels=gold_matrix))
+        clipper = 50
+        optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+        tvars = tf.trainable_variables()
+        l2_loss = tf.add_n([tf.nn.l2_loss(v) for v in tvars if v.get_shape().ndims > 1])
+        self.loss = self.loss + 1e-5 * l2_loss
+        grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars), clipper)
+        if self.options.decay:
+            self.optimizer = optimizer.apply_gradients(zip(grads, tvars), global_step=self.global_step)
+        else:
+            self.optimizer = optimizer.apply_gradients(zip(grads, tvars))
+
+        loss_summ = tf.summary.scalar('loss', self.loss)
+        self.train_summary.append(loss_summ)
+
+    def sequence_matching_no_p_split(self, n):
+        # p = tf.concat([self.P_states[0][0], self.P_states[0][1]], -1)  # [B, 2*H]
+
+        I_split = tf.split(self.I, 7, 1)
+        I_context = []
+        for i in range(7):
+            context, state = self.directional_lstm(I_split[i], [7 for _ in range(self.options.batch_size)], 2, self.options.hidden_size, 1, 'I_context' + str(i))
+            state = tf.concat([state[0][0], state[0][1]], -1) # fw + bw
+            # split_contextual_repr = tf.concat(context, -1)
+            I_context.append(tf.expand_dims(state, 1))
+        # import ipdb; ipdb.set_trace()  # TODO BREAKPOINT
+
+        I_full_context = tf.concat(I_context, 1) #B, 7, HH
+        _, I_embedding = self.directional_lstm(I_split[i], [7 for _ in range(self.options.batch_size)], 2, self.options.hidden_size, 1, 'I_embedding')
+        p = tf.concat([I_embedding[0][0], I_embedding[0][1]], -1)  # [B, 2*H]
+        # p = tf.reshape(p, (self.options.batch_size, -1))
+        #TODO remove W b
+        W_i = tf.get_variable("w_i", [FEAT_SIZE, 2*self.options.hidden_size], dtype=tf.float32)
+        b_i = tf.get_variable("b_i", [2*self.options.hidden_size], dtype=tf.float32)
+        p = tf.matmul(p, W_i) + b_i
+
+        q = tf.concat([self.H_states[0][0], self.H_states[0][1]], -1)  # [B, 2*H]
+        # p_shape = tf.shape(p)
+        # q_shape = tf.shape(q)
+        # p = tf.reshape(p, (-1, 2*self.options.hidden_size))
+        # q = tf.reshape(q, (-1, 2*self.options.hidden_size))
+        q_sub_p = p - q  # TODO try with module
+        q_mul_p = tf.multiply(p , q)
+        p = tf.expand_dims(p, axis=1)
+        q = tf.expand_dims(q, axis=1)
+        q_sub_p = tf.expand_dims(q_sub_p, axis=1)
+        q_mul_p = tf.expand_dims(q_mul_p, axis=1)
+        x_cl = tf.concat([p, q, q_sub_p, q_mul_p], 1)  # [B, 4, HH] #TODO add max e min pointwise
+        x_cl_flat = tf.reshape(x_cl, (self.options.batch_size*4, 2*self.options.hidden_size))
+        W_z= tf.get_variable("W_z", [n, 2*self.options.hidden_size], dtype=tf.float32)
+        b_z = tf.get_variable("b_z", [n], dtype=tf.float32)
+        z = tf.transpose(tf.matmul(W_z, tf.transpose(x_cl_flat))) + b_z
+        z = tf.reshape(z, (self.options.batch_size, -1))
+        W_score= tf.get_variable("W_score", [NUM_CLASSES, 4*n], dtype=tf.float32)
+        b_score = tf.get_variable("b_score", [NUM_CLASSES], dtype=tf.float32)
+        # score = tf.nn.xw_plus_b(tf.nn.relu(z), W_score, b_score)
+        self.score = tf.transpose(tf.matmul(W_score, tf.transpose(tf.nn.relu(z)))) + b_score
+
+        self.prob = tf.nn.softmax(self.score)
+        gold_matrix = tf.one_hot(self.labels, NUM_CLASSES, dtype=tf.float32)
+        self.loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.score, labels=gold_matrix))
+        clipper = 50
+        optimizer = tf.train.AdamOptimizer(learning_rate=self.learning_rate)
+        tvars = tf.trainable_variables()
+        l2_loss = tf.add_n([tf.nn.l2_loss(v) for v in tvars if v.get_shape().ndims > 1])
+        self.loss = self.loss + 1e-5 * l2_loss
+        grads, _ = tf.clip_by_global_norm(tf.gradients(self.loss, tvars), clipper)
+        if self.options.decay:
+            self.optimizer = optimizer.apply_gradients(zip(grads, tvars), global_step=self.global_step)
+        else:
+            self.optimizer = optimizer.apply_gradients(zip(grads, tvars))
+
+        loss_summ = tf.summary.scalar('loss', self.loss)
+        self.train_summary.append(loss_summ)
+
+    #TODO DELETE FUNC
+    def _____sequence_matching_no_p_top_down_split(self, n, lstm=False):
+        import ipdb; ipdb.set_trace()  # TODO BREAKPOINT
+        if not lstm:
+            cell_fw_H = tf.contrib.rnn.GRUBlockCellV2(256, name="fw_cells_H")
+            outputs, H_embedding = tf.nn.dynamic_rnn(cell_fw_H, self.H_lookup, dtype=tf.float32, swap_memory=True, sequence_length=self.lengths_H)
+        elif lstm:
+            cell_fw_H = tf.contrib.rnn.LSTMCell(256, name="fw_cells_H")
+            cell_bw_H = tf.contrib.rnn.LSTMCell(256, name="bw_cells_H")
+            outputs, H_embedding = tf.nn.bidirectional_dynamic_rnn(cell_fw_P, cell_bw_P, self.P_lookup, dtype=tf.float32, swap_memory=True)
+            H_embedding = tf.concat([H_embedding[0], H_embedding[1]], 1) #[batch_size x HH]
+
+
+        I_split = tf.split(self.I, 7, 1)
+        I_context = []
+        for i in range(7):
+            context, state = self.directional_lstm(I_split[i], [7 for _ in range(self.options.batch_size)], 2, self.options.hidden_size, 1, 'I_context')
+            state = tf.concat([state[0][0], state[0][1]], -1) # fw + bw
+            # split_contextual_repr = tf.concat(context, -1)
+            I_context.append(tf.expand_dims(state, 1))
+        import ipdb; ipdb.set_trace()  # TODO BREAKPOINT
+
+        I_full_context = tf.concat(I_context, 1) #B, 7, HH
+        _, H_embedding = self.directional_lstm(I_split[i], [7 for _ in range(self.options.batch_size)], 2, self.options.hidden_size, 1, 'I_context')
+        #For each location i = 1...K in the image, the feature
+        #vector v_i is concatenated with the question embedding q
+
+        feat_H = tf.map_fn(lambda i: tf.map_fn(lambda x: tf.concat([x, H_embedding[i]], 0), self.I[i]), tf.convert_to_tensor(list(range(self.options.batch_size)), dtype=tf.int32), dtype=tf.float32) #[batch_size x NUM_FEATS x (FEAT_SIZE + HH)]
+        #Passed through a non-linear layer f_a...
+        HH = self.options.hidden_size# * 2
+        N = 512
+        W_nl = tf.get_variable("W_nl", [N, FEAT_SIZE + HH], dtype=tf.float32)
+        b_nl = tf.get_variable("b_nl", [N], dtype=tf.float32)
+        feat_H = tf.reshape(tf.transpose(tf.to_float(feat_H), perm=[2,0,1]), [FEAT_SIZE + HH, -1]) #[FEAT_SIZE + HH, batch_size * NUM_FEATS]
+        y_att = tf.tanh(tf.transpose(tf.matmul(W_nl, feat_H)) + b_nl) # [batch_size x NUM_FEATS, N]
+        W_nl2 = tf.get_variable("W_nl2", [N, FEAT_SIZE + HH], dtype=tf.float32)
+        b_nl2 = tf.get_variable("b_nl2", [N], dtype=tf.float32)
+        g_att = tf.nn.sigmoid(tf.transpose(tf.matmul(W_nl2, feat_H)) + b_nl2) # [batch_size x NUM_FEATS, N]
+        y = tf.multiply(y_att, g_att)  # [batch_size x NUM_FEATS, N]
+        W_a = tf.get_variable("W_a", [1, FEAT_SIZE], dtype=tf.float32)
+        a = tf.matmul(W_a, tf.transpose(y)) # [1, batch_size x NUM_FEATS]
+        #...to obtain a scalar attention weight α_{i,t}
+        alpha = tf.nn.softmax(tf.transpose(a)) # [1, batch_size x NUM_FEATS]
+        alpha = tf.reshape(alpha, [self.options.batch_size, NUM_FEATS, 1])
+        #The image features are then weighted by the normalized values and summed
+        features_att = tf.map_fn(lambda x: tf.multiply(x[1], x[0]), (self.I, alpha), dtype=alpha.dtype) #[batch_size x NUM_FEATS x FEAT_SIZE]
+        self.features_att = tf.map_fn(lambda x: tf.reduce_sum(x, 0), features_att) #[batch_size x FEAT_SIZE]
+
+        # import ipdb; ipdb.set_trace()  # TODO BREAKPOINT
+        # p = tf.concat([self.P_states[0][0], self.P_states[0][1]], -1)  # [B, 2*H]
+
+        p = tf.reshape(self.features_att, (self.options.batch_size, -1))
+        # W_i = tf.get_variable("w_i", [FEAT_SIZE*NUM_FEATS, 2*self.options.hidden_size], dtype=tf.float32)
+        # b_i = tf.get_variable("b_i", [2*self.options.hidden_size], dtype=tf.float32)
+        # p = tf.matmul(p, W_i) + b_i
 
         q = tf.concat([self.H_states[0][0], self.H_states[0][1]], -1)  # [B, 2*H]
         # p_shape = tf.shape(p)
